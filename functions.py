c6adc0b3b588f8b6ba6c7a2469498ffeae5ee66b
Pablo <manbyak5@gmail.com>
2022-04-08 16:58:23 +0300

    edit html file

--- functions.py
+++ functions.py
@@ -1,1 +1,14 @@
 import json
+
+POST_PATH = "posts.json"
+
+
+def load_function_posts():
+    with open(POST_PATH, 'r', encoding='utf-8') as file:
+        posts = json.load(file)
+        return posts
+
+
+def uploads(posts):
+    with open(POST_PATH, 'w', encoding='utf-8') as file:
+        json.dump(posts, file)
